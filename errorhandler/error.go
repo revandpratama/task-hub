dd4d053e4ff7b47c9b31311346d72eba398b0d59
revandpratama <revandaustin@gmail.com>
2024-08-04 22:10:04 +0700

    initialize error handler

---
+++ errorhandler/error.go
@@ -0,0 +1,20 @@
+package errorhandler
+
+// import "net/http"
+
+// func HandleError(c, err error) {
+// 	var statusCode int
+
+// 	switch err.(type) {
+// 	case *NotFoundErr:
+// 		statusCode = http.StatusNotFound
+// 	case *InternalServerErr:
+// 		statusCode = http.StatusInternalServerError
+// 	case *UnauthorizedErr:
+// 		statusCode = http.StatusUnauthorized
+// 	case *BadRequestErr:
+// 		statusCode = http.StatusBadRequest
+// 	}
+
+	
+// }
