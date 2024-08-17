324a9858b96cf83c52e4f0101362ed55b894f39d
revandpratama <revandaustin@gmail.com>
2024-08-17 14:37:33 +0700

    setup database: postgresql

---
+++ src/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/revandpratama/task-hub/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DBCONN *gorm.DB
+
+func LoadDB() {
+	connStr := fmt.Sprintf(
+	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+	config.ENV.DB_URL,
+	config.ENV.DB_USERNAME,
+	config.ENV.DB_PASSWORD,
+	config.ENV.DB_NAME,
+	config.ENV.DB_PORT);
+
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	DBCONN = db
+
+	log.Println("Database running...")
+}
